92667d8d4bc283a89f16b8b7594526084a620f60
Sam Komarov <sam@skomarov.com>
2023-12-26 10:38:32 +0300

    move solutions to Practice.ipynb

--- practice/task_4.py
+++
@@ -1,6 +0,0 @@
-def gcd(a, b): 
-    if a == 0:
-        return b
-    return gcd(b%a, a)
-
-print(gcd(int(input()), int(input()))) 
--- practice/task_6.py
+++
@@ -1,7 +0,0 @@
-def is_prime(a):
-    for d in range(2, int(a**0.5)+1):
-        if a % d == 0:
-            return False 
-    return True
-
-print(is_prime(int(input())))
